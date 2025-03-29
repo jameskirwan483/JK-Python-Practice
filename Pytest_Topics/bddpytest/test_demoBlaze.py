38eb2e94d16c99be8aa5e16347c5d1336da56296
James_Kirwan <jameskirwan483@gmail.com>
2025-03-29 21:20:56 +0000

    Additional pop-up test and tagging tests

--- Pytest_Topics/bddpytest/test_demoBlaze.py
+++ Pytest_Topics/bddpytest/test_demoBlaze.py
@@ -43,3 +43,25 @@ def message_sent(driver):
     assert alert.text == expected_text, f"Unexpected alert text: {alert.text}"
     print("Assertion passed: The alert contains the expected text.")
 
+@scenario(str(FEATURE_FILE), 'Attempt to sign-up to Demo Blaze with email already used')
+def test_contact():
+    pass
+
+@given('I navigate to the Demo Blaze website')
+def demo_blaze_website(driver):
+    driver.get('https://www.demoblaze.com/index.html#')
+
+@when('I sign up with an email already used')
+def email_signup(driver):
+    driver.find_element(By.ID,'signin2').click()
+    driver.find_element(By.ID,'sign-username').send_keys("test")
+    driver.find_element(By.ID,'sign-password').send_keys("test")
+    driver.find_element(By.XPATH,'//*[@id="signInModal"]/div/div/div[3]/button[2]').click()
+
+@then('the sign-up is unsuccessful')
+def unsuccessful_signup (driver):
+    time.sleep(2)
+    alert = driver.switch_to.alert
+    expected_text = "This user already exist."
+    assert alert.text == expected_text, f"Unexpected alert text: {alert.text}"
+    print("Assertion passed: The alert contains the expected text.")
